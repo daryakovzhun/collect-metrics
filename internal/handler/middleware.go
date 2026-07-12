2ae6e74596c9eac99e7d976a0fd0573f27c7f344
darakovzun <dariakovzhun@gmail.com>
2026-07-12 16:49:12 +0300

    add zap logger

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,23 +1,32 @@
 package handler
 
 import (
-	"log/slog"
+	"github.com/daryakovzhun/collect-metrics/internal/logger"
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
 	"net/http"
 	"time"
 )
 
-func WithLogger(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		t1 := time.Now()
-		defer func() {
-			slog.Info(
-				"got incoming HTTP request",
-				slog.String("uri", r.RequestURI),
-				slog.String("method", r.Method),
-				slog.String("duration", time.Since(t1).String()),
-			)
-		}()
+func WithLogger() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
 
-		h.ServeHTTP(w, r)
-	})
+			t1 := time.Now()
+
+			next.ServeHTTP(ww, r)
+
+			logger.Log.Info(
+				"got incoming HTTP request",
+				zap.String("uri", r.RequestURI),
+				zap.String("method", r.Method),
+				zap.String("duration", time.Since(t1).String()),
+				zap.Int("status", ww.Status()),
+				zap.Int("size", ww.BytesWritten()),
+			)
+		}
+
+		return http.HandlerFunc(fn)
+	}
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -10,7 +10,7 @@ func New(h *handler.Handler) chi.Router {
 	r := chi.NewRouter()
 
 	r.Use(middleware.Recoverer)
-	r.Use(handler.WithLogger)
+	r.Use(handler.WithLogger())
 
 	r.Post("/update/{metric_type}/{metric_name}/{metric_value}", h.SetMetric)
 	r.Get("/value/{metric_type}/{metric_name}", h.GetMetric)
