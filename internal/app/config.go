4c6e8055dbbb1ab08e5a2ad73bd453b84f9ffa62
darakovzun <dariakovzhun@gmail.com>
2026-07-26 14:58:42 +0300

    fix configs

--- internal/app/config.go
+++ internal/app/config.go
@@ -25,28 +25,29 @@ type ServerConfig struct {
 func getAgentConfig() (*AgentConfig, error) {
 	var cfg AgentConfig
 
-	err := env.Parse(&cfg)
-	if err != nil {
+	addr := flag.String("a", "localhost:8080", "address and port to run server")
+	reportInterval := flag.Int("r", 10, "frequency of sending metrics to the server")
+	pollInterval := flag.Int("p", 2, "the frequency of polling metrics from the package")
+	key := flag.String("k", "", "the key to use for encryption")
+
+	flag.Parse()
+
+	if err := env.Parse(&cfg); err != nil {
 		return nil, err
 	}
 
 	if cfg.ServerAddress == "" {
-		flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "address and port to run server")
+		cfg.ServerAddress = *addr
 	}
-
 	if cfg.ReportInterval == 0 {
-		flag.IntVar(&cfg.ReportInterval, "r", 10, "frequency of sending metrics to the server")
+		cfg.ReportInterval = *reportInterval
 	}
-
 	if cfg.PollInterval == 0 {
-		flag.IntVar(&cfg.PollInterval, "p", 2, "the frequency of polling metrics from the package")
+		cfg.PollInterval = *pollInterval
 	}
-
 	if cfg.Key == "" {
-		flag.StringVar(&cfg.Key, "k", "", "the key to use for encryption")
+		cfg.Key = *key
 	}
-
-	flag.Parse()
 
 	return &cfg, nil
 }
@@ -54,38 +55,37 @@ func getAgentConfig() (*AgentConfig, error) {
 func getServerConfig() (*ServerConfig, error) {
 	var cfg ServerConfig
 
-	err := env.Parse(&cfg)
-	if err != nil {
+	addr := flag.String("a", "localhost:8080", "address and port to run server")
+	storeInterval := flag.Int("i", 300, "store interval")
+	filePath := flag.String("f", "metrics.txt", "path to store files")
+	restore := flag.Bool("r", false, "restore metrics")
+	db := flag.String("d", "", "database connection string")
+	key := flag.String("k", "", "the key to use for encryption")
+
+	flag.Parse()
+
+	if err := env.Parse(&cfg); err != nil {
 		return nil, err
 	}
 
 	if cfg.Address == "" {
-		flag.StringVar(&cfg.Address, "a", "localhost:8080", "address and port to run server")
+		cfg.Address = *addr
 	}
-
 	if cfg.StoreInterval == nil {
-		cfg.StoreInterval = utils.ToPointer(0)
-		flag.IntVar(cfg.StoreInterval, "i", 300, "store interval")
+		cfg.StoreInterval = utils.ToPointer(*storeInterval)
 	}
-
 	if cfg.FileStoragePath == "" {
-		flag.StringVar(&cfg.FileStoragePath, "f", "metrics.txt", "path to store files")
+		cfg.FileStoragePath = *filePath
 	}
-
 	if cfg.Restore == nil {
-		cfg.Restore = utils.ToPointer(false)
+		cfg.Restore = utils.ToPointer(*restore)
 	}
-	flag.BoolVar(cfg.Restore, "r", *cfg.Restore, "restore metrics")
-
 	if cfg.DB == "" {
-		flag.StringVar(&cfg.DB, "d", "", "database connection string")
+		cfg.DB = *db
 	}
-
 	if cfg.Key == "" {
-		flag.StringVar(&cfg.Key, "k", "", "the key to use for encryption")
+		cfg.Key = *key
 	}
-
-	flag.Parse()
 
 	return &cfg, nil
 }
